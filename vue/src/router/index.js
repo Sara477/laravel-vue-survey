27d9c990ea0a5b0b6d72498e04b3541928fcf27e
Sara <snenadic3@gmail.com>
2023-09-02 19:37:36 +0200

    Dashboard,Login i Register.vue

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 const routes=[
     {
-        path:''
+        path:'/',
+        name:'Dashboard',
+        component:Dashboard
+    },
+    {
+        path:'/login',
+        name:'Login',
+        component:Login
+    },
+    {
+        path:'/register',
+        name:'Register',
+        component:Register
     }
 ];
 
