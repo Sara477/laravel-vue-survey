1f1bce91ee1b1ee2ac562b4958c8e394350fba4f
Sara <snenadic3@gmail.com>
2023-09-02 19:44:21 +0200

    sredjivanje views

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from '../views/Dashboard.vue'
-import Login from '../views/Login.vue'
+import Dashboard from "../views/Dashboard.vue"
+import Login from "../views/Login.vue"
 import Register from '../views/Register.vue'
 
 const routes=[
